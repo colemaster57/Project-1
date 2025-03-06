7e35417c9a4d4519e38c1acdb6f08e2dc3403cf4
Cole Myers <colemyers2003@gmail.com>
2025-03-06 15:56:37 -0600

    initial commit

---
+++ src/App1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const App = () => {
+    return <h1> Hello, React is working!</h1>
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App1 />);
+
+export default App;
